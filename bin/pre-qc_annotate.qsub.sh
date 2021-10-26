bf9a4e011effa83963777d837d5b7d2b81043196
joseah <alquicirajose@gmail.com>
2021-10-26 21:19:21 +1100

    update again

--- bin/pre-qc_annotate.qsub.sh
+++ bin/pre-qc_annotate.qsub.sh
@@ -6,6 +6,8 @@
 #$ -M j.alquicira@garvan.org.au
 #$ -m ae
 #$ -cwd 
+#$ -o results/2021-10-26_pre-qc_annotation
+#$ -e results/2021-10-26_pre-qc_annotation
 
 # qsub -t 1-3 bin/pre-qc_annotate.qsub.sh
 
@@ -19,17 +21,17 @@ input=results/2021-10-26_raw_seurat_objects
 output=results/2021-10-26_pre-qc_annotation
 
 # Get job info
-echo "JOB: $JOB_ID TASK: $SGE_TASK_ID"
-echo "$HOSTNAME $tmp_requested $TMPDIR"
+echo "JOB: $JOB_ID TASK: $SGE_TASK_ID HOSTNAME: $HOSTNAME"
 
 # Get basefile name
-
 files=($(ls ${input}))
 i="$(($JOB_ID-1))"
+
+echo "i = $i"
 
 filename=${files[$i]}
 
 echo "Running for: $filename"
 
 # Run main command
-singularity run -B $SGE_O_WORKDIR bin/azimuth.sif --file ${input}/${filename}.RDS --out ${output}/${filename}_out
+#singularity run -B $SGE_O_WORKDIR bin/azimuth.sif --file ${input}/${filename}.RDS --out ${output}/${filename}_out
