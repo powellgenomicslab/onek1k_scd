36e0e20622cbcd8fa3618152cc5f75fa824a9a12
joseah <alquicirajose@gmail.com>
2021-10-26 21:08:07 +1100

    updates paths

--- bin/pre-qc_annotate.qsub.sh
+++ bin/pre-qc_annotate.qsub.sh
@@ -1,19 +1,22 @@
 #$ -N annotate_pools
 #$ -q short.q
 #$ -l mem_requested=16G
-#$ -cwd
 #$ -S /bin/bash
 #$ -r yes
 #$ -M j.alquicira@garvan.org.au
 #$ -m ae
+#$ -cwd 
 
-# qsub -t 1-3 ../../bin/pre-qc_annotate.qsub.sh
+# qsub -t 1-3 bin/pre-qc_annotate.qsub.sh
+
+cd $SGE_O_WORKDIR
 
 # Set up environment
 conda activate r-4.1.1
 
 # Set environmental variables
-input=../2021-10-26_raw_seurat_objects
+input=results/2021-10-26_raw_seurat_objects
+output=results/2021-10-26_pre-qc_annotation
 
 # Get job info
 echo "JOB: $JOB_ID TASK: $SGE_TASK_ID"
@@ -29,4 +32,4 @@ filename=${files[$i]}
 echo "Running for: $filename"
 
 # Run main command
-singularity run -B $PWD ../../bin/azimuth.sif --file ${input}/${filename}.RDS --out ${filename}_out
+singularity run -B $SGE_O_WORKDIR bin/azimuth.sif --file ${input}/${filename}.RDS --out ${output}/${filename}_out
