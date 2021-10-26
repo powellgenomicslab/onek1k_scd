5916016b8e2cc9adf59cbd5397beb8bb00feb3af
joseah <alquicirajose@gmail.com>
2021-10-26 20:47:08 +1100

    fixes input path

--- bin/pre-qc_annotate.qsub.sh
+++ bin/pre-qc_annotate.qsub.sh
@@ -9,11 +9,13 @@
 #$ -M j.alquicira@garvan.org.au
 #$ -m ae
 
+# qsub -t 1-3 ../../bin/pre-qc_annotate.qsub.sh
+
 # Set up environment
 conda activate r-4.1.1
 
 # Set environmental variables
-input=results/2021-10-26_raw_seurat_objects
+input=../2021-10-26_raw_seurat_objects
 
 # Get job info
 echo "JOB: $JOB_ID TASK: $SGE_TASK_ID"
@@ -22,8 +24,11 @@ echo "$HOSTNAME $tmp_requested $TMPDIR"
 # Get basefile name
 
 files=($(ls ${input}))
+i="$(($JOB_ID-1))"
 
 filename=${files[$i]}
+
+echo "Running for: $filename"
 
 # Run main command
 singularity run -B $PWD bin/azimuth.sif --file ${input}/${filename}.RDS --out ${filename}_out
