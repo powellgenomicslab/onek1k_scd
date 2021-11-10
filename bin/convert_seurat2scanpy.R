ef3e6cc3dfc53896cd38c991a9affd52831737f2
joseah <alquicirajose@gmail.com>
2021-11-10 16:13:52 +1100

    adds script to convert seurat to scanpy data

---
+++ bin/convert_seurat2scanpy.R
@@ -0,0 +1,50 @@
+#   ____________________________________________________________________________
+#   Script information                                                      ####
+
+# title: Convert onek1k data to h5ad file
+# author: Jose Alquicira Hernandez
+# date: 2021-11-10
+# description: Converts onek1k data to h5ad file
+
+#   ____________________________________________________________________________
+#   HPC details                                                             ####
+
+# screen -S h5ad
+# qrsh -N h5ad -l mem_requested=200G -pe smp 1 -q short.q
+# conda activate r-4.1.1
+
+#   ____________________________________________________________________________
+#   Import libraries                                                        ####
+
+library("dsLib")
+library("Seurat")
+library("SeuratDisk")
+
+#   ____________________________________________________________________________
+#   Set output                                                              ####
+
+output <- here("results", "2021-11-10_h5ad")
+dir.create(output)
+
+#   ____________________________________________________________________________
+#   Import data                                                             ####
+
+data <- LoadH5Seurat(here("results", "2021-11-10_add_metadata", "onek1k.h5seurat"),
+                     assays = list(RNA = "counts"), 
+                     reductions = c("harmony", "azimuth_spca", "azimuth_umap"))
+
+#   ____________________________________________________________________________
+#   Export data                                                             ####
+
+SaveH5Seurat(data, filename = here(output, "onek1k.h5Seurat"), overwrite = TRUE)
+Convert(here(output, "onek1k.h5Seurat"), dest = "h5ad", overwrite = TRUE)
+
+#   ____________________________________________________________________________
+#   Session info                                                            ####
+
+print_session(here(output))
+
+
+
+
+
